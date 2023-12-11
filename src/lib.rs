ab965446ea38b04cb96d0cb0db2fdf3fdde72d40
Frits Hoogland <frits.hoogland@gmail.com>
2023-12-11 15:47:27 +0100

    all cpu modes

--- src/lib.rs
+++ src/lib.rs
@@ -43,6 +43,11 @@ pub async fn read_proc_data() -> ProcData
 
 pub async fn process_data(proc_data: ProcData, statistics: &mut HashMap<(String, String), Statistic>)
 {
+    process_cpu_data(proc_data, statistics).await;
+}
+
+pub async fn process_cpu_data(proc_data: ProcData, statistics: &mut HashMap<(String, String), Statistic>)
+{
     statistics.entry(("cpu".to_string(), "user".to_string()))
         .and_modify(|row|{
             row.delta_value = proc_data.stat.cpu_total.user as f64 - row.last_value;
@@ -57,7 +62,161 @@ pub async fn process_data(proc_data: ProcData, statistics: &mut HashMap<(String,
                 last_value: proc_data.stat.cpu_total.user as f64,
                 delta_value: 0.0,
                 per_second_value: 0.0,
-                new_value: false,
+                new_value: true,
             }
         );
+    statistics.entry(("cpu".to_string(), "nice".to_string()))
+        .and_modify(|row|{
+            row.delta_value = proc_data.stat.cpu_total.nice as f64 - row.last_value;
+            row.per_second_value = row.delta_value / (proc_data.timestamp.signed_duration_since(row.last_timestamp).num_milliseconds() as f64 / 1000_f64);
+            row.last_value = proc_data.stat.cpu_total.nice as f64;
+            row.last_timestamp = proc_data.timestamp;
+            row.new_value = false;
+        })
+        .or_insert(
+            Statistic {
+                last_timestamp: proc_data.timestamp,
+                last_value: proc_data.stat.cpu_total.nice as f64,
+                delta_value: 0.0,
+                per_second_value: 0.0,
+                new_value: true,
+            }
+        );
+    statistics.entry(("cpu".to_string(), "system".to_string()))
+        .and_modify(|row|{
+            row.delta_value = proc_data.stat.cpu_total.system as f64 - row.last_value;
+            row.per_second_value = row.delta_value / (proc_data.timestamp.signed_duration_since(row.last_timestamp).num_milliseconds() as f64 / 1000_f64);
+            row.last_value = proc_data.stat.cpu_total.system as f64;
+            row.last_timestamp = proc_data.timestamp;
+            row.new_value = false;
+        })
+        .or_insert(
+            Statistic {
+                last_timestamp: proc_data.timestamp,
+                last_value: proc_data.stat.cpu_total.system as f64,
+                delta_value: 0.0,
+                per_second_value: 0.0,
+                new_value: true,
+            }
+        );
+    statistics.entry(("cpu".to_string(), "idle".to_string()))
+        .and_modify(|row|{
+            row.delta_value = proc_data.stat.cpu_total.idle as f64 - row.last_value;
+            row.per_second_value = row.delta_value / (proc_data.timestamp.signed_duration_since(row.last_timestamp).num_milliseconds() as f64 / 1000_f64);
+            row.last_value = proc_data.stat.cpu_total.idle as f64;
+            row.last_timestamp = proc_data.timestamp;
+            row.new_value = false;
+        })
+        .or_insert(
+            Statistic {
+                last_timestamp: proc_data.timestamp,
+                last_value: proc_data.stat.cpu_total.idle as f64,
+                delta_value: 0.0,
+                per_second_value: 0.0,
+                new_value: true,
+            }
+        );
+    statistics.entry(("cpu".to_string(), "iowait".to_string()))
+        .and_modify(|row|{
+            row.delta_value = proc_data.stat.cpu_total.iowait as f64 - row.last_value;
+            row.per_second_value = row.delta_value / (proc_data.timestamp.signed_duration_since(row.last_timestamp).num_milliseconds() as f64 / 1000_f64);
+            row.last_value = proc_data.stat.cpu_total.iowait as f64;
+            row.last_timestamp = proc_data.timestamp;
+            row.new_value = false;
+        })
+        .or_insert(
+            Statistic {
+                last_timestamp: proc_data.timestamp,
+                last_value: proc_data.stat.cpu_total.iowait as f64,
+                delta_value: 0.0,
+                per_second_value: 0.0,
+                new_value: true,
+            }
+        );
+    statistics.entry(("cpu".to_string(), "irq".to_string()))
+        .and_modify(|row|{
+            row.delta_value = proc_data.stat.cpu_total.irq as f64 - row.last_value;
+            row.per_second_value = row.delta_value / (proc_data.timestamp.signed_duration_since(row.last_timestamp).num_milliseconds() as f64 / 1000_f64);
+            row.last_value = proc_data.stat.cpu_total.irq as f64;
+            row.last_timestamp = proc_data.timestamp;
+            row.new_value = false;
+        })
+        .or_insert(
+            Statistic {
+                last_timestamp: proc_data.timestamp,
+                last_value: proc_data.stat.cpu_total.irq as f64,
+                delta_value: 0.0,
+                per_second_value: 0.0,
+                new_value: true,
+            }
+        );
+    statistics.entry(("cpu".to_string(), "softirq".to_string()))
+        .and_modify(|row|{
+            row.delta_value = proc_data.stat.cpu_total.softirq as f64 - row.last_value;
+            row.per_second_value = row.delta_value / (proc_data.timestamp.signed_duration_since(row.last_timestamp).num_milliseconds() as f64 / 1000_f64);
+            row.last_value = proc_data.stat.cpu_total.softirq as f64;
+            row.last_timestamp = proc_data.timestamp;
+            row.new_value = false;
+        })
+        .or_insert(
+            Statistic {
+                last_timestamp: proc_data.timestamp,
+                last_value: proc_data.stat.cpu_total.softirq as f64,
+                delta_value: 0.0,
+                per_second_value: 0.0,
+                new_value: true,
+            }
+        );
+    statistics.entry(("cpu".to_string(), "steal".to_string()))
+        .and_modify(|row|{
+            row.delta_value = proc_data.stat.cpu_total.steal as f64 - row.last_value;
+            row.per_second_value = row.delta_value / (proc_data.timestamp.signed_duration_since(row.last_timestamp).num_milliseconds() as f64 / 1000_f64);
+            row.last_value = proc_data.stat.cpu_total.steal as f64;
+            row.last_timestamp = proc_data.timestamp;
+            row.new_value = false;
+        })
+        .or_insert(
+            Statistic {
+                last_timestamp: proc_data.timestamp,
+                last_value: proc_data.stat.cpu_total.steal as f64,
+                delta_value: 0.0,
+                per_second_value: 0.0,
+                new_value: true,
+            }
+        );
+    statistics.entry(("cpu".to_string(), "guest".to_string()))
+        .and_modify(|row|{
+            row.delta_value = proc_data.stat.cpu_total.guest as f64 - row.last_value;
+            row.per_second_value = row.delta_value / (proc_data.timestamp.signed_duration_since(row.last_timestamp).num_milliseconds() as f64 / 1000_f64);
+            row.last_value = proc_data.stat.cpu_total.guest as f64;
+            row.last_timestamp = proc_data.timestamp;
+            row.new_value = false;
+        })
+        .or_insert(
+            Statistic {
+                last_timestamp: proc_data.timestamp,
+                last_value: proc_data.stat.cpu_total.guest as f64,
+                delta_value: 0.0,
+                per_second_value: 0.0,
+                new_value: true,
+            }
+        );
+    statistics.entry(("cpu".to_string(), "guest_nice".to_string()))
+        .and_modify(|row|{
+            row.delta_value = proc_data.stat.cpu_total.guest_nice as f64 - row.last_value;
+            row.per_second_value = row.delta_value / (proc_data.timestamp.signed_duration_since(row.last_timestamp).num_milliseconds() as f64 / 1000_f64);
+            row.last_value = proc_data.stat.cpu_total.guest_nice as f64;
+            row.last_timestamp = proc_data.timestamp;
+            row.new_value = false;
+        })
+        .or_insert(
+            Statistic {
+                last_timestamp: proc_data.timestamp,
+                last_value: proc_data.stat.cpu_total.guest_nice as f64,
+                delta_value: 0.0,
+                per_second_value: 0.0,
+                new_value: true,
+            }
+        );
+
 }
