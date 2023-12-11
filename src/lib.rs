ae6e454b7760e251c8d9e4ce9692527e324d5ccc
Frits Hoogland <frits.hoogland@gmail.com>
2023-12-11 15:38:06 +0100

    cpu user mode

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
 use proc_sys_parser::{schedstat, stat, meminfo, diskstats, net_dev};
+use chrono::{DateTime, Local};
+use std::collections::HashMap;
 
 #[derive(Debug)]
 pub struct ProcData
 {
+    pub timestamp: DateTime<Local>,
     pub stat: stat::ProcStat,
     pub schedstat: schedstat::ProcSchedStat,
     pub meminfo: meminfo::ProcMemInfo,
@@ -10,18 +13,51 @@ pub struct ProcData
     pub net_dev: net_dev::ProcNetDev,
 }
 
+#[derive(Debug)]
+pub struct Statistic
+{
+    pub last_timestamp: DateTime<Local>,
+    pub last_value: f64,
+    pub delta_value: f64,
+    pub per_second_value: f64,
+    pub new_value: bool,
+}
+
 pub async fn read_proc_data() -> ProcData
 {
+    let timestamp = Local::now();
     let proc_stat = stat::read();
     let proc_schedstat = schedstat::read();
     let proc_meminfo = meminfo::read();
     let proc_diskstats = diskstats::read();
     let proc_netdev = net_dev::read();
     ProcData {
+        timestamp: timestamp,
         stat: proc_stat,
         schedstat: proc_schedstat,
         meminfo: proc_meminfo,
         diskstats: proc_diskstats,
         net_dev: proc_netdev,
     }
+}
+
+pub async fn process_data(proc_data: ProcData, statistics: &mut HashMap<(String, String), Statistic>)
+{
+    statistics.entry(("cpu".to_string(), "user".to_string()))
+        .and_modify(|row|{
+            row.delta_value = proc_data.stat.cpu_total.user as f64 - row.last_value;
+            row.per_second_value = row.delta_value / (proc_data.timestamp.signed_duration_since(row.last_timestamp).num_milliseconds() as f64 / 1000_f64);
+            row.last_value = proc_data.stat.cpu_total.user as f64;
+            row.last_timestamp = proc_data.timestamp;
+            row.new_value = false;
+        })
+        .or_insert(
+            Statistic {
+                last_timestamp: proc_data.timestamp,
+                last_value: proc_data.stat.cpu_total.user as f64,
+                delta_value: 0.0,
+                per_second_value: 0.0,
+                new_value: false,
+            }
+        );
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
-use proc_sys_parser;
+//use proc_sys_parser;
 use time::Duration;
 use tokio::time;
+use std::collections::HashMap;
 
-use procstat::read_proc_data;
+use procstat::{read_proc_data, process_data, Statistic};
 #[tokio::main]
 async fn main()
 {
     let mut interval = time::interval(Duration::from_secs(2));
+    let mut statistics: HashMap<(String, String), Statistic> = HashMap::new();
     loop
     {
         interval.tick().await;
 
         let data = read_proc_data().await;
+        process_data(data, &mut statistics).await;
 
-        println!("{:?}", data);
+        println!("{:?}", statistics);
     }
 }
