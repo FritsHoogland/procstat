70b913d26d2214985d3cd21212ad534daa0d2834
Frits Hoogland <frits.hoogland@gmail.com>
2023-12-13 22:11:23 +0100

    cpu-all

--- src/stat.rs
+++ src/stat.rs
@@ -66,8 +66,8 @@ pub async fn print_cpu(statistics: &HashMap<(String, String, String), Statistic>
     let guest_nice = statistics.get(&("stat".to_string(), "all".to_string(), "guest_nice".to_string())).unwrap().delta_value;
     let idle = statistics.get(&("stat".to_string(), "all".to_string(), "idle".to_string())).unwrap().delta_value;
     let total = user+nice+system+iowait+steal+irq+softirq+guest_user+guest_nice+idle;
-    let scheduler_running = statistics.get(&("schedstat".to_string(), "all".to_string(), "time_running".to_string())).unwrap().per_second_value/1000000_f64;
-    let scheduler_waiting = statistics.get(&("schedstat".to_string(), "all".to_string(), "time_waiting".to_string())).unwrap().per_second_value/1000000_f64;
+    let scheduler_running = statistics.get(&("schedstat".to_string(), "all".to_string(), "time_running".to_string())).unwrap().delta_value/1000000_f64;
+    let scheduler_waiting = statistics.get(&("schedstat".to_string(), "all".to_string(), "time_waiting".to_string())).unwrap().delta_value/1000000_f64;
     match output
     {
         "sar-u" => {
